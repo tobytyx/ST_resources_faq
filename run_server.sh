b3d2a14a626d27f78e8ffcfa63d973883fd59514
tanyanxin <tobytyx@outlook.com>
2021-02-25 14:28:30 +0800

    取消reload

--- run_server.sh
+++ run_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm ./output/uvicorn.log || true
-nohup uvicorn server:app --port 9900 --host 0.0.0.0 --reload >> ./output/uvicorn.log 2>&1 &
+nohup uvicorn server:app --port 9900 --host 0.0.0.0 >> ./output/uvicorn.log 2>&1 &
