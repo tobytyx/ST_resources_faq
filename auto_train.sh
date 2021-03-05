f43b6c1776283bc3aa5976eac75fb41d89a1a029
tanyanxin <tobytyx@outlook.com>
2021-03-05 10:59:19 +0800

    增加自动训练的log

--- auto_train.sh
+++ auto_train.sh
@@ -3,4 +3,4 @@ CUDA_VISIBLE_DEVICES=2 nohup python auto_train.py \
 --record_id $1 \
 --name $2 \
 --domain $3 \
---data_path $4 >> /dev/null 2>&1 &
+--data_path $4 >> ./output/auto_train.log 2>&1 &
